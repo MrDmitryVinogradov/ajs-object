b8d59a5c8a94b4ef529a28003b24681e353f58d5
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-12-01 21:47:27 +0300

    Fix

--- src/js/sorting.js
+++ src/js/sorting.js
@@ -1,24 +1,25 @@
 export default function sorting(obj, keys) {
   let copy = [];
-  for (let i = 0; i < keys.length; i++) {
+  keys.forEach((element) => {
     for (let prop in obj) {
-      if (keys[i] === prop) {
+      if (element === prop) {
         copy.push({ 'key': prop, 'value': obj[prop] });
         delete obj[prop];
       }
-    }
-  }
+    };
+  });
+
   let arr = [];
   for (let prop in obj) {
     arr.push(prop);
   }
   arr.sort();
-  for (let i = 0; i < arr.length; i++) {
+  arr.forEach((element) => {
     for (let prop in obj) {
-      if (arr[i] === prop) {
+      if (element === prop) {
         copy.push({ 'key': prop, 'value': obj[prop] });
       }
     }
-  }
+  });
   return copy
 }
